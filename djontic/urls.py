2e9d89a349e4afc332ded3c6c2866dcf2c69765d
onticinc <onticinc@gmail.com>
2022-03-22 14:30:27 -0600

    eggs

--- djontic/urls.py
+++ djontic/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('mixes/', include('mixes.urls')),
     path('events/', include('events.urls')),
+    path('eggs/', include('eggs.urls')),
 ]
